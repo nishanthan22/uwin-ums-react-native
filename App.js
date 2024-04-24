6eaac6c1a1b4097f02987cc7358a809ebffdaccf
Nishanthan M Ravichandran <nishanthravi226@gmail.com>
2024-04-23 23:34:40 -0400

    device configuration success

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Successfully tested web, android and ios devices</Text>
+      <Text>Working perfectly on all devices :)</Text>
       <StatusBar style="auto" />
     </View>
   );
